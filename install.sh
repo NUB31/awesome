cbc7d72a5a177332b298930b172b3c8c901d9a0c
Oliver Stene <59693115+NUB31@users.noreply.github.com>
2023-05-11 12:33:24 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ~
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
+cd ~
+rm -r yay-bin
+
+yay -S awesome-git sddm pulsemixer playerctl xdg-utils xclip feh maim xdotool rofi picom
+sudo systemctl enable sddm
+
+cd $SCRIPT_DIR
+mkdir ~/.config
+mkdir ~/.config/awesome
+mkdir ~/.config/picom
+mkdir ~/.config/alacritty
+cp -r awesome/* ~/.config/awesome/*
+cp -r picom/* ~/.config/picom/*
+cp -r alacritty/* ~/.config/alacritty/*
+
+cd
