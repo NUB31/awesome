c89acbbac048f60d2e54d0e67dd48bbe48d2597e
nub31 <oliverlhs@outlook.com>
2023-06-04 02:28:52 +0200

    no more rice, just plain old awesome

--- awesome/widget/mebox/templates/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-    item = require("widget.mebox.templates.item"),
-    header = require("widget.mebox.templates.header"),
-    separator = require("widget.mebox.templates.separator"),
-}
--- install.sh
+++ install.sh
@@ -1,24 +1,14 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd ~
-pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
 
-cd ~
-rm -r yay-bin
-
-yay -S awesome-git sddm pulsemixer playerctl xdg-utils xclip feh maim xdotool rofi picom
+sudo apt install -y awesome sddm rofi thunar firefox
 sudo systemctl enable sddm
 
 cd $SCRIPT_DIR
 mkdir ~/.config
 mkdir ~/.config/awesome
-mkdir ~/.config/picom
-mkdir ~/.config/alacritty
 cp -r awesome/* ~/.config/awesome/*
-cp -r picom/* ~/.config/picom/*
-cp -r alacritty/* ~/.config/alacritty/*
+cp "50-mouse-acceleration.conf" /usr/share/X11/xorg.conf.d/50-mouse-acceleration.conf
 
 cd
