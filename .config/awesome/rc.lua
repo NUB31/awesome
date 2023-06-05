37c0c66ee4c7308e10ea4e6161b0608e0d9c902d
Oliver Stene <59693115+NUB31@users.noreply.github.com>
2023-06-05 16:13:41 +0200

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -56,7 +56,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "kitty"
 editor = "code"
 fileBrowser = "thunar"
 webBrowser = "firefox"
