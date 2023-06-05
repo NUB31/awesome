1b07565d0a9add08c5435044a6536b27f6bc2448
Oliver Stene <59693115+NUB31@users.noreply.github.com>
2023-06-05 18:17:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y xorg awesome lightdm feh picom thunar
+echo "deb http://deb.debian.org/debian bookworm main non-free-firmware\ndeb-src http://deb.debian.org/debian bookworm main non-free-firmware\n\ndeb http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware\ndeb-src http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware\n\ndeb http://deb.debian.org/debian bookworm-updates main non-free-firmware\ndeb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" > /etc/apt/sources.list
+
+apt update -y
+apt upgrade -y
+apt install -y xorg awesome lightdm feh picom
 
 # optional
-sudo apt install -y firefox-esr kitty lxappearance git gedit thunderbird
+apt install -y firefox-esr kitty lxappearance git thunderbird nautilus gedit
 
 cp -r ./.config ~/
 cp -r ./.wallpapers ~/
 cp -r ./.icons ~/
 
-sudo mkdir /usr/share/cursors
-sudo mkdir /usr/share/cursors/xorg-x11
-sudo ln -s ~/.icons /usr/share/cursors/xorg-x11/
+mkdir /usr/share/cursors
+mkdir /usr/share/cursors/xorg-x11
+ln -s ~/.icons /usr/share/cursors/xorg-x11/
