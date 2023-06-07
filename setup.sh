75ac45520ce5d3df844f05563258ace0a25d5d7f
Oliver Stene <59693115+NUB31@users.noreply.github.com>
2023-06-07 14:23:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,17 +16,6 @@ do
   fi; 
 done
 
-if ! command -v yay &> /dev/null
-then
-  echo "yay is not installed. Starting yay installation"
-  pacman --noconfirm -S git
-  cd /opt
-  git clone https://aur.archlinux.org/yay-git.git
-  chown -R $USER ./yay-git
-  cd yay-git
-  makepkg --asroot -si
-fi
-
 echo "Updating installed packages"
 yay -Syu --noconfirm
 
